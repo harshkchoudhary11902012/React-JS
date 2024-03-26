7dfbb77399bec1d8cfc25650f47c7c690c002c75
harshkchoudhary <harsh.c@libratherm.com>
2024-03-26 17:55:23 +0530

    Custom React Elements

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,10 @@
 import Demo from "./demo";
 function App() {
+	const username = "harsh choudhary";
 	return (
 		<>
 			<Demo />
-			<h1>Vite + React</h1>
+			<h1>Vite + React + {username}</h1>
 			<p>Using React Js and Vite together to build a website application</p>
 		</>
 	);
